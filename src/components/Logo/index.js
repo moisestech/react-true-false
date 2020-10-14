e05bab8b12d75bde416ad3554312d36331a395c4
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-14 16:06:42 -0400

    func comp w/ Logo test and init slogan

---
+++ src/components/Logo/index.js
@@ -0,0 +1,12 @@
+export default function Logo() {
+  return (
+    <div className="logo-header">
+      <span className="logo">
+        <img src="" />
+      </span>
+      <p>
+        <i>True +/- False Challenge</i>
+      </p>
+    </div>
+  );
+}
