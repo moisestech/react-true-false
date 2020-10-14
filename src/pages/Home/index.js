41bc1ae0e9b4d7f5ca57537cc46549b072d4d452
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-14 16:20:23 -0400

    init func comp Home displaying start challenge quiz navigation

---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import { NavLink } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <NavLink to="" exact className="start">
+        Start Challenge!!!
+      </NavLink>
+    </div>
+  );
+}
