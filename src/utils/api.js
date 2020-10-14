6d5f1da84af972b6d6711704a61838f375fdbd6a
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-14 17:33:27 -0400

    query fetch quizURL opentdb.com/api.php, getQuestions getInitialData

---
+++ src/utils/api.js
@@ -0,0 +1,35 @@
+
+const quizURL = "https://opentdb.com/api.php"
+
+function getQuestions(quizURL, amount, difficulty, type) {
+  const URL = `${quizURL}?amount=${amount}&difficulty=${difficulty}&type=${type}`;
+  return fetch(URL)
+    .then((res) => res.json())
+    .then((json) => ({
+      questions: json.results
+    }));
+
+    // const results = json.results
+    //   console.log("json.results", results)
+    //   console.log("data", data);
+    //   return results
+
+  // async () => {
+  //   const data = await fetch(quizURL);
+  //   console.log("data", data);
+  //   const questions = await data.json();
+  //   console.log("getQuestions()", questions.results);
+  //   return questions.results;
+  // };
+}
+
+export function getInitialData() {
+  return new Promise((res, rej) => {
+    res(getQuestions(quizURL, 10, "hard", "boolean"))
+  })
+
+  // return new Promise((response, reject) => {
+  //   response(getQuestions(quizURL));
+  //   reject("Error in Promise getQuestions utils/api.js");
+  // });
+}
