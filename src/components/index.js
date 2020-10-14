13091bb5d9fcf0f2407026f5c9a527271369ab62
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-14 16:09:54 -0400

    init func comp <App/> w/ switch routes and passing init store w/ useSelector hook after useEffect dispatch handleInitialdata

---
+++ src/components/index.js
@@ -0,0 +1,45 @@
+import { useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { handleInitialData } from "../state/actions/questions";
+
+// COMPONENTS
+import Home from "../pages/Home";
+import Quiz from "../pages/Quiz";
+import NotFound from "../pages/NotFound";
+import Nav from "../components/Nav";
+
+export default function App() {
+  const dispatch = useDispatch();
+  // const loading = useSelector((state) => state.questions === null);
+
+  useEffect(() => {
+    dispatch(handleInitialData());
+  }, [dispatch]);
+
+  const store = useSelector((store) => store);
+
+  console.log("store", store);
+
+  return (
+    <Router>
+      <div className="app container">
+        <Nav />
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/quiz">
+            <Quiz questions={store.questions}/>
+          </Route>
+
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
