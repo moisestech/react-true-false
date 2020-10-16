892d3574fb717e44694937322a6f8985a6dbd4a0
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-15 21:44:32 -0400

    generateID, parseQuote

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,1 +1,10 @@
-console.log("helper function files!");
+export function generateID() {
+  return (
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15)
+  );
+}
+
+export function parseQuote (text) {
+  return text.replace(/&quot;/g,'"')
+}
