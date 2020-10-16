1bfbc1bd33aee3b844f1661b5dd474beaa3c3960
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-15 21:38:13 -0400

    comp Home clName .welcome, NavLink Begin

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,9 +3,16 @@ import { NavLink } from "react-router-dom";
 export default function Home() {
   return (
     <div className="home">
-      <NavLink to="" exact className="start">
-        Start Challenge!!!
-      </NavLink>
+
+      <div className="welcome">
+        <div>Welcome to the Trivia Challenge!</div>
+        <div>You will be presented with 10 True or False questions.</div>
+        <div>Can you score 100?</div>
+        <NavLink exact to="/quiz?number=1" className="start-quiz">
+          Begin
+        </NavLink>
+      </div>
+      
     </div>
   );
 }
