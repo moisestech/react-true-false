441ad160dba22f0603084de6df6e9dba35628781
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-15 21:43:30 -0400

    combineReducers questions, answers, loadingBar

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
+import answers from './answers';
 import questions from "./questions";
 import { loadingBarReducer } from "react-redux-loading-bar";
 
 export default combineReducers({
   questions,
+  answers,
   loadingBar: loadingBarReducer,
 });
