76ac10fc2dfc40bab4ea79f8146486b37b4c939d
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-16 21:56:05 -0400

    ANSWER_QUESTION, RESET_ANSWERS actionStrings, actionCreator answerQuestion resetAnswers, actionHandler handleAnswerQuestion

--- src/state/actions/answers.js
+++ src/state/actions/answers.js
@@ -1,15 +1,22 @@
-export const ANSWER_QUESTION = "ANSWER_QUESTION"
+export const ANSWER_QUESTION = "ANSWER_QUESTION";
+export const RESET_ANSWERS = "RESET_ANSWERS";
 
 function answerQuestion(questionNumber, answer) {
   return {
     type: ANSWER_QUESTION,
     questionNumber,
-    answer
-  }
+    answer,
+  };
+}
+
+export function resetAnswers() {
+  return {
+    type: RESET_ANSWERS,
+  };
 }
 
 export function handleAnswerQuestion(questionNumber, answer, cb) {
   return (dispatch) => {
-    dispatch(answerQuestion(questionNumber, answer))
-  }
+    dispatch(answerQuestion(questionNumber, answer));
+  };
 }
