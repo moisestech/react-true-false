db0fe98551f8c1e5987fd1524430547575eb5423
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-16 21:57:58 -0400

    imp ANSWER_QUESTION return obj questionNumb answerResult RESET_ANSWER state = []

--- src/state/reducers/answers.js
+++ src/state/reducers/answers.js
@@ -1,13 +1,17 @@
-import { ANSWER_QUESTION } from '../../state/actions/answers';
+import { ANSWER_QUESTION, RESET_ANSWERS } from "../../state/actions/answers";
 
 // answers reducer
 export default function answers(state = [], action) {
   switch (action.type) {
     case ANSWER_QUESTION:
-      return state.concat([{
-        questionNumb: action.questionNumber,
-        answerResult: action.answer
-      }])
+      return state.concat([
+        {
+          questionNumb: action.questionNumber,
+          answerResult: action.answer,
+        },
+      ]);
+    case RESET_ANSWERS:
+      state = [];
     default:
       return state;
   }
