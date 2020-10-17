f9e5d584aea1af8ecb2771d1185da45618b9697c
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-16 21:58:52 -0400

    answers reducer in combineReducers

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 
-import answers from './answers';
+import answers from "./answers";
 import questions from "./questions";
 import { loadingBarReducer } from "react-redux-loading-bar";
 
