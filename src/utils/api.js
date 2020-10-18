44c771693f08233e8e967c06cb42d9163513abbf
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-17 21:37:54 -0400

    Prettier support

--- src/utils/api.js
+++ src/utils/api.js
@@ -7,28 +7,10 @@ function getQuestions(quizURL, amount, difficulty, type) {
     .then((json) => ({
       questions: json.results,
     }));
-
-  // const results = json.results
-  //   console.log("json.results", results)
-  //   console.log("data", data);
-  //   return results
-
-  // async () => {
-  //   const data = await fetch(quizURL);
-  //   console.log("data", data);
-  //   const questions = await data.json();
-  //   console.log("getQuestions()", questions.results);
-  //   return questions.results;
-  // };
 }
 
 export function getInitialData() {
   return new Promise((res, rej) => {
     res(getQuestions(quizURL, 10, "hard", "boolean"));
   });
-
-  // return new Promise((response, reject) => {
-  //   response(getQuestions(quizURL));
-  //   reject("Error in Promise getQuestions utils/api.js");
-  // });
 }
