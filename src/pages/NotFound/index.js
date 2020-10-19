336e3a94b98ef6f2289830e227ab9909817de47e
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-18 21:34:52 -0400

    imp GlowButton comp, imp ./notfound.css

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,9 +1,13 @@
+import "./notfound.css";
+import { NavLink } from "react-router-dom";
 import GlowButton from "../../components/GlowButton";
 export default function NotFound() {
   return (
     <div className="not-found-container">
       <h1>Sorry, we couldn't find what you're looking for.</h1>
-      <GlowButton text={"PLAY GAME"} />
+      <NavLink to={"/"}>
+        <GlowButton text={"PLAY GAME"} />
+      </NavLink>
     </div>
   );
 }
