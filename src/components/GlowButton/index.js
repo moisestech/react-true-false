124c7fd4ecdae20ee903191d8dfaa19a0f9de26b
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-18 21:20:51 -0400

    func comp used in Home, Result, 404

--- src/components/GlowButton/index.js
+++ src/components/GlowButton/index.js
@@ -1,5 +1,10 @@
+import "./glowbutton.css";
 import PropTypes from "prop-types";
 
 export default function GlowButton({ text }) {
   return <div className="glow-button">{text}</div>;
 }
+
+GlowButton.proptypes = {
+  text: PropTypes.string.isRequired,
+};
