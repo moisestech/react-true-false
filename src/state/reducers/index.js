e389285c9b5daeda8b92aaf4ab8a2c945fedc5ac
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-19 22:15:41 -0400

    combineReducers include typing

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 
 import answers from "./answers";
 import questions from "./questions";
+import typing from "./typing";
 import { loadingBarReducer } from "react-redux-loading-bar";
 
 export default combineReducers({
   questions,
   answers,
+  typing,
   loadingBar: loadingBarReducer,
 });
