eac0f08c759c82858f5c1ca8f24d6111846d14f7
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-20 23:54:45 -0400

    adding quiz, typing reducers

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from "redux";
 import answers from "./answers";
 import questions from "./questions";
 import typing from "./typing";
+import quiz from "./quiz";
 import { loadingBarReducer } from "react-redux-loading-bar";
 
 export default combineReducers({
   questions,
   answers,
   typing,
+  quiz,
   loadingBar: loadingBarReducer,
 });
