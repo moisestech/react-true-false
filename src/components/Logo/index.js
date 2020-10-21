ed8c17f72d1717546cdf42ef572ceff297b9bcb6
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-20 23:39:45 -0400

    activeLoaction class for logo in quiz

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,9 +1,15 @@
 import "./logo.css";
-export default function Logo() {
+export default function Logo({ activeLocation }) {
+  function handleActiveLocation(path) {
+    if (path === "/quiz") {
+      return "active";
+    }
+  }
+
   return (
     <div className="logo-header">
-      <span className="logo">
-        <img src="https://raw.githubusercontent.com/moisestech/react-true-false/main/public/assets/react-truefalse-logo.png" />
+      <span className={`logo ${handleActiveLocation(activeLocation)}`}>
+        <img src="https://raw.githubusercontent.com/moisestech/react-true-false/main/public/assets/react-truefalse-logo-square.png" />
       </span>
     </div>
   );
