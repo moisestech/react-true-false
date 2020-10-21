e72a30cd731a72ee9af8ab51373008aa81dd9d81
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-20 23:55:10 -0400

    ramdomizer function utils helper for Emoji func comp

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -8,3 +8,9 @@ export function generateID() {
 export function parseQuote(text) {
   return text.replace(/&quot;/g, '"');
 }
+
+export function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
