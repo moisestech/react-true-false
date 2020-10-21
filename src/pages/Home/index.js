021d66a616468b75572a2b16a134b7abc0946e9f
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-20 23:50:46 -0400

    className intro instructions challenge for better ui navigation

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,9 +6,11 @@ export default function Home() {
   return (
     <div className="home">
       <div className="welcome">
-        <div>Welcome to the Trivia Challenge!</div>
-        <div>You will be presented with 10 True or False questions.</div>
-        <div>Can you score 100?</div>
+        <div className="intro">Welcome to the Trivia Challenge!</div>
+        <div className="instructions">
+          You will be presented with 10 True or False questions.
+        </div>
+        <div className="challenge">Can you score 100?</div>
         <NavLink exact to="/quiz?number=1" className="start-quiz">
           <GlowButton text={"Begin"} />
         </NavLink>
