11ab643fe17ec4359795f325acb4fbbaacd29979
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-20 23:51:13 -0400

    notfound container emoji header <GlowButton/>

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,12 +1,19 @@
 import "./notfound.css";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import GlowButton from "../../components/GlowButton";
 export default function NotFound() {
+  const { pathname } = useLocation();
+
   return (
-    <div className="not-found-container">
-      <h1>Sorry, we couldn't find what you're looking for.</h1>
-      <NavLink to={"/"}>
-        <GlowButton text={"PLAY GAME"} />
+    <div className="notfound-container">
+      <div className="notfound-emoji">
+        <img src="https://raw.githubusercontent.com/moisestech/react-true-false/main/public/assets/not-found.gif" />
+      </div>
+      <h1 className="notfound-header">
+        Sorry, we couldn't find what you're looking for.
+      </h1>
+      <NavLink to={"/"} className="notfound-restart">
+        <GlowButton text={"RESTART"} />
       </NavLink>
     </div>
   );
